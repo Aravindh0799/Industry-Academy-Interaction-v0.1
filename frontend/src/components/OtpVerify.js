07f8bb1ca3d5d2055f63182a179ce07c8173f95d
aravindh0799 <aravindhan0799@gmail.com>
2023-05-15 00:57:47 +0530

    removed for loop in otp page

--- frontend/src/components/OtpVerify.js
+++ frontend/src/components/OtpVerify.js
@@ -15,8 +15,8 @@ const OtpVerify = ()=>{
     const[fourth,setFourth]=useState('')
     const[fifth,setFifth]=useState('')
     const[sixth,setSixth]=useState('')
-    const inputs = document.querySelectorAll('#otp > *[id]');
-    for (let i = 0; i < inputs.length; i++) { inputs[i].addEventListener('keydown', function(event) { if (event.key==="Backspace" ) { inputs[i].value='' ; if (i !==0) inputs[i - 1].focus(); } else { if (i===inputs.length - 1 && inputs[i].value !=='' ) { return true; } else if (event.keyCode> 47 && event.keyCode < 58) { inputs[i].value=event.key; if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } else if (event.keyCode> 64 && event.keyCode < 91) { inputs[i].value=String.fromCharCode(event.keyCode); if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } } }); } 
+    // const inputs = document.querySelectorAll('#otp > *[id]');
+    // for (let i = 0; i < inputs.length; i++) { inputs[i].addEventListener('keydown', function(event) { if (event.key==="Backspace" ) { inputs[i].value='' ; if (i !==0) inputs[i - 1].focus(); } else { if (i===inputs.length - 1 && inputs[i].value !=='' ) { return true; } else if (event.keyCode> 47 && event.keyCode < 58) { inputs[i].value=event.key; if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } else if (event.keyCode> 64 && event.keyCode < 91) { inputs[i].value=String.fromCharCode(event.keyCode); if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } } }); } 
     
     const resend= async(e)=>{
         const result = await axios.post('https://iai-v1.onrender.com/createotp', {
